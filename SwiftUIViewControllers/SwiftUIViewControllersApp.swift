a862515b92b06dd1398a4bd39deae03069b58004
Eяw!и Gontier <ierwinx@me.com>
2022-10-31 10:29:22 -0600

    Initial Commit

---
+++ SwiftUIViewControllers/SwiftUIViewControllersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewControllersApp.swift
+//  SwiftUIViewControllers
+//
+//  Created by Erwin Luz León on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewControllersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
